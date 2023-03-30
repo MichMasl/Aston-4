ea6543bb824b30579c460ca1ec88646bdfd027a7
Michael Maslennikov <mih.mass@yandex.ru>
2023-03-30 19:23:15 +0500

    initial commit

---
+++ app/src/main/java/ru/mihmas/aston_4/ClockView.kt
@@ -0,0 +1,14 @@
+package ru.mihmas.aston_4
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClockView(
+    context: Context?,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): View(context, attrs, defStyleAttr) {
+
+
+}
